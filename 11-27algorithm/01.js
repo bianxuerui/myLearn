782ac58f7b8736696bfbd4bea0016c5831f28bff
BianXuerui <407955981@qq.com>
2021-11-27 21:31:20 +0800

    合并K个升序链表

---
+++ 11-27algorithm/01.js
@@ -0,0 +1,69 @@
+
+/**
+ * 23. 合并K个升序链表
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+
+ /**
+  * =======
+  * 1.暴力破解
+  * =======
+  */
+var mergeKLists = function(lists) {
+    let arr = [];
+    for (let i=0; i<lists.length; i++) {
+        while (lists[i]) {
+            arr.push(lists[i].val);
+            lists[i] = lists[i].next;
+        }
+    }
+    arr.sort((a,b) => a - b);
+    let dummy = new ListNode(0);
+    let cur = dummy;
+    for (let i=0; i<arr.length; i++) {
+        cur.next = new ListNode(arr[i]);
+        cur = cur.next;
+    }
+    return dummy.next;
+};
+
+/**
+ * ====
+ * 2.分治
+ * ====
+ */
+var mergeKLists = function(lists) {
+    return merge(lists, 0, lists.length - 1);
+
+    function merge(lists, begin, end){
+        if (begin == end) return lists[begin];
+        if (begin > end) return null;
+
+        let mid = (begin + end) >> 1;
+        let l1 = merge(lists, begin, mid);
+        let l2 = merge(lists, mid + 1, end);
+        return mergeTwoLists(l1, l2);
+    }
+
+    function mergeTwoLists(l1,l2){
+        if (l1 == null || l2 == null) {
+            return l1 != null ? l1 : l2
+        }
+        let dummy = new ListNode(0);
+        let cur = dummy;
+        while (l1 && l2) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+};
+

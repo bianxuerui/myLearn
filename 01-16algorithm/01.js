b8553ab52e6029edb0409208a8e73694678e9b7f
BianXuerui <407955981@qq.com>
2022-01-16 16:14:19 +0800

    451. 根据字符出现频率排序

---
+++ 01-16algorithm/01.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+  let res = "";
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1);
+    } else {
+      map.set(s[i], 1);
+    }
+  }
+
+  let arr = [...map];
+  arr.sort((a, b) => b[1] - a[1]);
+  for (let [val, times] of arr) {
+    res += val.repeat(times);
+  }
+  return res;
+};
